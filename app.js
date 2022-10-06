dae23fe97ab99d130c2326ac71b683b0ea947643
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2022-10-06 13:21:59 +0530

    commit

--- app.js
+++ app.js
@@ -1,45 +1,49 @@
 const express = require("express");
-const app = express()
+const app = express();
 const cors = require("cors");
 
 require("dotenv").config();
 const mongoose = require("mongoose");
+//const cors = require("cors");
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-
 //require
+ 
 const user = require("./routes/user")
 const category = require("./routes/category")
 const subcategory = require("./routes/subcategory")
 
-
-
+ 
 //use
 app.use("/", user);
 app.use("/", category);
 app.use("/", subcategory);
-
-
 app.get("/", (req, res) => {
-    res.send("Hello World!");
-  });
+  res.send("Hello World!");
+});
 
-  mongoose.connect(process.env.DB,{
+//console.log(process.env.DB);
+mongoose
+  .connect(process.env.DB, {
     useNewUrlParser: true,
+    // useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }).then(()=>{
+    //useFindAndModify: false,
+  })
+  .then(() => {
     console.log("DB CONNECTED SUCCEFULLY");
-  }).catch((error) => {
+  })
+  .catch((error) => {
     console.log(error);
   });
+  
 
-  app.listen(process.env.PORT || 9000, ()=>{
-    console.log("Example app listening on port 9000");
-  })
+app.listen(process.env.PORT || 9000, () => {
+  console.log("Example app listening on port 9000");
+});
 
-
- // http://localhost:5000/admin
+//    http://localhost:5000/admin
